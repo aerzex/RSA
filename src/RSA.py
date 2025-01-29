bf839bd8088442d9805ec4898b233e9cbe574cf1
aerzex <aerzex@bk.ru>
2025-01-29 21:43:37 +1000

    Update key usage

--- src/RSA.py
+++ src/RSA.py
@@ -52,12 +52,13 @@ def generate_keys(length):
         "PKCS7CertChain-PKCS": 0
     }
 
-    # password = "P@ssw0rd"  
-    # save_keys_windows_format(pub_key, scrt_key, password)
+    password = "P@ssw0rd"  
+    save_keys_windows_format(pub_key, scrt_key, password)
 
     return pub_key, scrt_key
 
-def encrypt(pub_key, message):
+def encrypt(message):
+    pub_key = load_public_key_from_pem("RSA/src/rsa_keys/pub_key.pem")
     N = pub_key["SubjectPublicKeyInfo"]["N"]
     block_size = (N.bit_length() + 7) // 8
     max_msg_len = block_size - 3 - 8
@@ -74,19 +75,18 @@ def encrypt(pub_key, message):
         padded_block = padding + block
         int_block = int.from_bytes(padded_block, 'big')
         enc_block = algorithm_fast_pow(int_block, pub_key["SubjectPublicKeyInfo"]["publicExponent"], N)
-        print(enc_block)
         enc_blocks.append(enc_block)
 
     return enc_blocks
     
 
-def decrypt(scrt_key, enc_message):
+def decrypt(password, enc_message):
+    scrt_key = load_private_key_from_pfx("RSA/src/rsa_keys/key_store.pfx", password)
     N = scrt_key["prime1"] * scrt_key["prime2"]
     block_size = (N.bit_length() + 7) // 8
     dec_blocks = []
     for block in enc_message:
         dec_m = algorithm_fast_pow(block, scrt_key["privateExponent"], N)
-        print(f'dec_m: {dec_m}')
 
         dec_block = dec_m.to_bytes(block_size, byteorder='big')
         index = dec_block.find(b'\x00', 2)
@@ -130,32 +130,71 @@ def save_keys_windows_format(pub_key, scrt_key, password, filename="RSA/src/rsa_
         .not_valid_after(datetime.utcnow() + timedelta(days=365))\
         .sign(private_key, hashes.SHA256(), default_backend())
 
-    # Экспорт в PKCS#12 (для Windows)
+    # export in PKCS#12 (Windows)
     p12_data = pkcs12.serialize_key_and_certificates(
-        name=b"key",  # Имя контейнера
+        name=b"key",  # container name
         key=private_key,
         cert=cert,
-        cas=None,  # Дополнительные сертификаты
+        cas=None,  # extra certs
         encryption_algorithm=serialization.BestAvailableEncryption(password.encode())
     )
 
     with open(filename, "wb") as f:
         f.write(p12_data)
 
-    # Экспорт открытого ключа в PEM
     with open("RSA/src/rsa_keys/pub_key.pem", "wb") as f:
         f.write(public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ))
 
+
+def load_private_key_from_pfx(filename, password):
+    with open(filename, "rb") as f:
+        pfx_data = f.read()
+
+    private_key, cert, additional_certs = pkcs12.load_key_and_certificates(
+        pfx_data, password.encode(), default_backend()
+    )
+
+    private_numbers = private_key.private_numbers()
+    
+    scrt_key = {
+        "privateExponent": private_numbers.d,
+        "prime1": private_numbers.p,
+        "prime2": private_numbers.q,
+        "exponent1": private_numbers.dmp1,
+        "exponent2": private_numbers.dmq1,
+        "coefficient": private_numbers.iqmp
+    }
+    return scrt_key
+
+def load_public_key_from_pem(filename):
+    with open(filename, "rb") as f:
+        public_key = serialization.load_pem_public_key(f.read(), backend=default_backend())
+
+    public_numbers = public_key.public_numbers()
+    
+    pub_key = {
+        "SubjectPublicKeyInfo": {
+            "publicExponent": public_numbers.e,
+            "N": public_numbers.n
+        },
+        "PKCS10CertRequest": 0,
+        "Certificate": 0,
+        "PKCS7CertChain-PKCS": 0
+    }
+    return pub_key
+
+
 def main():
     size = int(input("Enter size of N: "))
     message_en = "If I don’t like a thing, I don’t like it, that’s all; and there is no reason under the sun why I should ape a liking for it just because the majority of my fellow-creatures like it, or make believe they like it. I can’t follow the fashions in the things I like or dislike."
     message_ru = "Если мне что-то не нравится, значит, не нравится, и все тут; так с какой стати, спрашивается, я стану делать вид, будто мне это нравится, только потому, что большинству моих соплеменников это нравится или они воображают, что нравится. Не могу я что-то любить или не любить по велению моды."
     pub_key, scrt_key = generate_keys(size)
-    enc_message = encrypt(pub_key, message_ru)
-    dec_message = decrypt(scrt_key, enc_message)
+    password = "P@ssw0rd"
+    enc_message = encrypt(message_ru)
+    dec_message = decrypt(password, enc_message)
     print(enc_message)
     print(dec_message)
 
